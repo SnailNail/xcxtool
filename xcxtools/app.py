90e0f1c153811e14cdf918e02b61d1d0aa598bbe
SnailNail <30770374+SnailNail@users.noreply.github.com>
2023-02-26 18:42:41 +0000

    Added application config module with defaults
    Config module is initialised by the main application

--- xcxtools/app.py
+++ xcxtools/app.py
@@ -1,21 +1,26 @@
 """Main entry point for xcxtools"""
+from configparser import ConfigParser
+from typing import Mapping
 
 from plumbum import cli
 import plumbum
+
 from . import VERSION
+from . import config
 
 
 class XCXToolsCLI(cli.Application):
     PROGNAME = "xcxtools"
     DESCRIPTION = "Utilities for playing Xenoblade Chronicles X on Cemu"
     VERSION = VERSION
-    config_path = cli.SwitchAttr(
+
+    config_path: plumbum.LocalPath = cli.SwitchAttr(
         "--config-path",
         plumbum.local.path,
         help="Use this file for config instead of the default",
         default=plumbum.local.path("xcxtools.ini"),
     )
-    cemu_process_name = cli.SwitchAttr(
+    cemu_process_name: str = cli.SwitchAttr(
         "--cemu-process-name",
         help="Name of the Cemu process to read data from",
         default="cemu.exe"
@@ -23,9 +28,4 @@ class XCXToolsCLI(cli.Application):
 
     def main(self):
         print("Running XCXToolCLI.main()")
-        print(f"Using config at {self.config_path}")
-        print(
-            f"Config file exists"
-            if self.config_path.exists()
-            else "Config file does not exist"
-        )
+        config.load_config(self.config_path)
---
+++ xcxtools/config.py
@@ -0,0 +1,77 @@
+"""Configuration module
+
+The app (including subcommands) is intended to be configured through a config
+file named xcxtool.ini in the working directory. Settings can be overridden
+on the command line. If a setting isn't in the config file or given on the
+command line, default values are used.
+"""
+
+import configparser
+import os
+
+from plumbum import LocalPath
+
+CONFIG_DEFAULTS = {
+    "cemu": {
+        "save_path": r"c:\cemu\mlc01\usr\save\00050000\101c4c00",
+        "user_id": "80000001",
+        "process_name": "cemu.exe"
+    },
+    "backup": {
+        "path": r".\saves",
+        "file_name": "",
+    },
+    "sightseeing_spots": {
+        # Primordia
+        "FN101": 1,
+        "FN103": 1,
+        "FN104": 1,
+        "FN106": 1,
+        "FN110": 1,
+        "FN117": 1,
+        # Noctilum
+        "FN213": 1,
+        "FN214": 2,
+        "FN216": 1,
+        "FN220": 1,
+        "FN221": 2,
+        "FN222": 1,
+        "FN223": 1,
+        "FN225": 1,
+        # Oblivia
+        "FN306": 1,
+        "FN313": 2,
+        "FN315": 2,
+        "FN317": 1,
+        "FN318": 2,
+        "FN319": 1,
+        # Sylvalum
+        "FN404": 1,
+        "FN408": 1,
+        "FN410": 1,
+        "FN413": 1,
+        "FN414": 2,
+        "FN419": 1,
+        # Cauldros
+        "FN502": 1,
+        "FN503": 1,
+        "FN505": 2,
+        "FN506": 1,
+        "FN507": 1,
+        "FN508": 1,
+        "FN513": 2,
+        "FN514": 1,
+    },
+}
+
+config = configparser.ConfigParser()
+config.read_dict(CONFIG_DEFAULTS)
+
+
+def load_config(config_path: LocalPath):
+    """load configuration from file"""
+    print(f"Using config file: {config_path}")
+    if not config_path.exists():
+        print("Config file not found")
+        return
+    config.read(config_path)
