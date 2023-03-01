15fd4f0df54004aae62ee1ab7ab4e7f52b145fcb
SnailNail <30770374+SnailNail@users.noreply.github.com>
2023-03-01 23:59:49 +0000

    Define CLI for save backup tool

--- xcxtools/backup_save.py
+++ xcxtools/backup_save.py
@@ -1,17 +1,32 @@
 """Backup Cemu save file"""
-from typing import Mapping
-
 import plumbum
 from plumbum import cli
 
-from . import config
+from .config import CONFIG_DEFAULTS, config
 
 
 class BackupSave(cli.Application):
+    backup_dir: cli.ExistingDirectory = cli.SwitchAttr(
+        ["-d", "--backup-dir"],
+        help=f"Backups will be saved to this directory; default is '{CONFIG_DEFAULTS['backup']['path']}'",
+    )
+    backup_name: str = cli.SwitchAttr(
+        ["-f", "--file"],
+        help=f"Name for the backup file; default is '{CONFIG_DEFAULTS['backup']['file_name']}'",
+    )
+    save_dir: cli.ExistingDirectory = cli.SwitchAttr(
+        ["-s", "--save-dir"],
+        help="XCX save directory. Can be found by selecting 'Save directory' from the "
+             "game's context menu in Cemu's game list",
+    )
+    user_id: str = cli.SwitchAttr(
+        ["-u", "--user-id"],
+        help=f"Cemu/WiiU user account ID; default is '{CONFIG_DEFAULTS['cemu']['user_id']}'",
+    )
 
     def main(self):
-        save_path = config.config["cemu"]["save_path"]
-        user_id = config.config["cemu"]["user_id"]
+        save_path = config["cemu"]["save_path"]
+        user_id = config["cemu"]["user_id"]
 
         save_path = plumbum.local.path(save_path, "user", user_id)
         if save_path.exists():
@@ -19,3 +34,6 @@ class BackupSave(cli.Application):
         else:
             print(f"Save path does not exist: {save_path}")
 
+    @cli.switch(["--help-names"])
+    def help_names(self):
+        """Print available replacement tokens for --backup-path and --file"""
