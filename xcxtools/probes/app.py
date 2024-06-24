59d4f9d1887ddd6b6260ae915b8148182a464f25
SnailNail <30770374+SnailNail@users.noreply.github.com>
2024-06-24 16:52:36 +0100

    Start implementing actual command

--- xcxtools/probes/app.py
+++ xcxtools/probes/app.py
@@ -2,19 +2,21 @@
 
 from collections import Counter
 
+import plumbum
 from plumbum import cli, LocalPath
 
-from xcxtools import savefiles
+from xcxtools import savefiles, config
 from xcxtools.probes import data
 
 
 class FrontierNavTool(cli.Application):
 
-    def main(self):
-        print("Nothing to see here, yet")
+    def main(self, target: cli.ExistingFile = None):
+        if target is None:
+            print(f"Using config file")
 
 
-def get_save_data(file_path: LocalPath) -> bytes:
+def get_save_data_from_file(file_path: LocalPath) -> bytes:
     """Helper function to get save data"""
     if file_path.stat().st_size != 359984:
         raise ValueError("Savefile should be exactly 359,984 bytes")
@@ -22,6 +24,13 @@ def get_save_data(file_path: LocalPath) -> bytes:
     raw_data: bytes = file_path.read(mode="b")
     key = savefiles.guess_key(raw_data)
     return savefiles.apply_key(raw_data, key)
+
+
+def get_save_data_from_backup_folder() -> bytes:
+    """Get save data from the backup folder config"""
+    folder = plumbum.local.path(config.get("backup.save_directory"))
+    save_file = folder.join("st", "game", "gamedata")
+    return get_save_data_from_file(save_file)
 
 
 def get_probe_inventory(probe_inventory_buffer: bytes) -> Counter[data.Probe]:
