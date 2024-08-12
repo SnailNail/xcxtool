431eebf0405bf9c3e600cf90d13308013160f0e6
SnailNail <30770374+SnailNail@users.noreply.github.com>
2024-08-12 13:56:03 +0100

    Replace config system to use TOML instead of INI

---
+++ xcxtools/config/__init__.py
@@ -0,0 +1,1 @@
+from xcxtools.config.main import *
---
+++ xcxtools/config/defaults.py
@@ -0,0 +1,57 @@
+"""Default configuration values/config schema"""
+CONFIG_DEFAULTS = {
+    "cemu": {
+        "process_name": "cemu.exe"
+    },
+    "backup": {
+        "save_directory": r"C:\cemu\mlc01\usr\save\00050000\101c4d00\user\80000001",
+        "backup_directory": r".\saves",
+        "file_name": "backup-{datetime}",
+    },
+    "sightseeing_spots": {
+        # Primordia
+        "101": 1,
+        "103": 1,
+        "104": 1,
+        "106": 1,
+        "110": 1,
+        "117": 1,
+        # Noctilum
+        "213": 1,
+        "214": 2,
+        "216": 1,
+        "220": 1,
+        "221": 2,
+        "222": 1,
+        "223": 1,
+        "225": 1,
+        # Oblivia
+        "306": 1,
+        "313": 2,
+        "315": 2,
+        "317": 1,
+        "318": 2,
+        "319": 1,
+        # Sylvalum
+        "404": 1,
+        "408": 1,
+        "410": 1,
+        "413": 1,
+        "414": 2,
+        "419": 1,
+        # Cauldros
+        "502": 1,
+        "503": 1,
+        "505": 2,
+        "506": 1,
+        "507": 1,
+        "508": 1,
+        "513": 2,
+        "514": 1,
+    },
+    "monitor": {
+        "obs_port": 4455,
+        "obs_host": "localhost",
+        "obs_password": "",
+    }
+}
