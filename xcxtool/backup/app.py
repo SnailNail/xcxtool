581c1e4782b2b01a304cb6010d5aefaa78db4d44
SnailNail <30770374+SnailNail@users.noreply.github.com>
2024-08-19 15:00:16 +0100

    Basically rewrite backup app to (hopefully) be more readable

--- xcxtool/backup/app.py
+++ xcxtool/backup/app.py
@@ -1,5 +1,6 @@
 """Backup Cemu save file"""
 import shutil
+import sys
 
 import plumbum
 from plumbum import local, cli
@@ -23,82 +24,87 @@ class BackupSave(cli.Application):
         argtype=plumbum.LocalPath,
         help=f"Backups will be saved to this directory",
     )
-    create_backup_dir: bool = cli.Flag(
-        ["-c", "--create"],
-        help="Try to create backup-dir if it doesn't exist",
-    )
     backup_name: str = cli.SwitchAttr(
         ["-f", "--file"],
         help=f"Name for the backup file (without extension)",
     )
-    help_tokens: bool = cli.Flag("--help-tokens", help="Show available replacement tokens")
     save_dir: plumbum.LocalPath = cli.SwitchAttr(
         ["-s", "--save-dir"],
         argtype=cli.ExistingDirectory,
-        help="Directory containing XCX save files in the st/game subdirectory",
+        help="Custom save data path. Must contain save files in the st/game subdirectory",
     )
     dry_run: bool = cli.Flag(["--dry-run"], help="Do not create the archive, just print its name")
     gamedata: plumbum.LocalPath = None
 
     def main(self):
-        if self._help_names():
-            return
-        if result := self.pre_flight_checks() is not None:
-            return result
-        if self.backup_name is None:
-            self.backup_name = config.get("backup.file_name")
+        if self.parent is None:
+            print("This utility must be run via the main xcxtool application", file=sys.stderr)
+            return 2
 
+        backup_path = self.get_backup_path()
+        if backup_path is None:
+            return 2
+
+        save_path = self.get_save_path()
+        if save_path is None:
+            rprint("[red]No save data path specified[/red]", file=sys.stderr)
+            return 2
+
+        save_data_path = save_path.join("st", "game", "gamedata")
+        if not save_data_path.exists():
+            rprint(f"[red]No save data found in {save_path}", file=sys.stderr)
+            return 2
+
+        reader = memory_reader.SaveFileReader(save_data_path)
+        field_values = self.get_tokens(reader)
+        archive_name = formatter.ForgivingFormatter().format(self.backup_name, **field_values)
+
+        self.do_backup(archive_name, backup_path, save_path)
+
+    def get_backup_path(self) -> plumbum.LocalPath | None:
+        if self.backup_dir is None:
+            return self.backup_dir
+
+        backup_dir = plumbum.local.path(config.get("backup.backup_directory"))
+        if not self.backup_dir.exists():
+            rprint(f"[red]Backup directory not found (got '{self.backup_dir}')")
+            return
+        if not self.backup_dir.is_dir():
+            rprint(f"[red]Backup path is not a directory (got '{self.backup_dir}')")
+            return
+        return backup_dir
+
+    def get_save_path(self) -> plumbum.LocalPath | None:
+        if self.save_dir is not None:
+            return self.save_dir
+
+        if configured := config.get("backup.save_directory") is not None:
+            return plumbum.local.path(configured)
+
+        if self.parent.cemu_save_dir is not None:
+            return self.parent.cemu_save_dir.parents[-1]
+
+    def get_tokens(self, gamedata_reader: memory_reader.MemoryReader) -> dict:
         field_values = {}
         field_values.update(tokens.get_datetime())
-
         field_values.update(tokens.get_mtime(self.gamedata))
 
-        gamedata_reader = memory_reader.SaveFileReader(self.gamedata)
         field_values.update(tokens.get_character_data(gamedata_reader))
         field_values.update(tokens.get_playtime(gamedata_reader))
+        return field_values
 
-        archive_name = formatter.ForgivingFormatter().format(self.backup_name, **field_values)
-        rprint(f"Backing up from: [green]{self.save_dir}[/green]")
-        rprint(f"to: [green]{self.backup_dir}[/green]")
-        rprint(f"With filename [green]{archive_name}.zip[/green]")
-        base_name = self.backup_dir / archive_name
+    def do_backup(self, backup_name: str, backup_dir: plumbum.LocalPath, save_dir: plumbum.LocalPath):
+        rprint(f"Backing up from: [green]{self.save_dir}[/green]\n"
+               f"to: [green]{backup_dir}[/green]\n"
+               f"With filename [green]{backup_name}.zip[/green]")
+        base_name = backup_dir / backup_name
         if not self.dry_run:
             shutil.make_archive(
-                base_name, "zip", self.save_dir, "st/game", dry_run=self.dry_run
+                base_name, "zip", save_dir, "st/game", dry_run=self.dry_run
             )
 
-    def pre_flight_checks(self) -> int | None:
-        """Various checks before running main() proper"""
-        if not any((self._check_parent(), self._check_save_path(), self._check_backup_path())):
-            return 2
-        return
-
-    def _check_save_path(self) -> bool:
-        if self.save_dir is None:
-            self.save_dir = local.path(config.get("backup.save_directory"))
-        gamedata = self.save_dir.join("st", "game", "gamedata")
-        if not gamedata.exists():
-            print(f"Could not find gamedata in {self.save_dir}")
-            return False
-        self.gamedata = gamedata
-        return True
-
-    def _check_backup_path(self) -> bool:
-        if self.backup_dir is None:
-            self.backup_dir = local.path(config.get("backup.backup_directory"))
-        if self.backup_dir.exists():
-            return True
-        if self.create_backup_dir:
-            return self.backup_dir.mkdir()
-        return False
-
-    def _check_parent(self) -> bool:
-        if self.parent:
-            return True
-        print("This utility must be run via the main xcxtool application")
-        return False
-
-    def _help_names(self):
+    @cli.switch("--help-tokens", help="Show available replacement tokens")
+    def help_names(self):
         """Print available replacement tokens for --backup-path and --file"""
         preamble = ("The --backup-dir and --file arguments can contain replacement fields "
                     "e.g, 'fixed_{player_name}_also_fixed'. The available fields are "
@@ -115,7 +121,6 @@ class BackupSave(cli.Application):
             "{level}": "Inner level of the player character (int)",
             "{exp}": "Total inner experience points for player character (int)",
             "{class}": "Player's combat class (str)",
-            "{class_level}": "Player's class level (int)",
             "{class_rank}": "Player's class rank (int)",
             "{class_exp}": "Total class exp of the player (int)",
             "{play_time}": "Total playtime as seen on the main menu. "
@@ -125,9 +130,8 @@ class BackupSave(cli.Application):
             "{save_date}": "Last modified date/time of the gamedata save file (pendulum.datetime)",
             "{date}": "Current (calendar) date/time (pendulum.datetime)",
         }
-        if self.help_tokens:
-            print(preamble)
-            for token, description in token_descriptions.items():
-                print(f"  {token:14} {description}")
-            return True
-        return False
+        print(preamble)
+        for token, description in token_descriptions.items():
+            rprint(f"  {token:14} {description}")
+        sys.exit()
+
