c0445b7d961a37a52b3ef732149c34276e6bed3a
SnailNail <30770374+SnailNail@users.noreply.github.com>
2024-09-10 13:47:36 +0100

    Add generic function for loading JSON data with a single text column
    Also differentiated data and text dump folders, and added setters for both

--- xcxtool/data/load_json.py
+++ xcxtool/data/load_json.py
@@ -1,6 +1,6 @@
 import functools
 import json
-from os.path import dirname, abspath, join
+from os.path import dirname, exists, abspath, join
 
 try:
     _this_dir = dirname(abspath(__file__))
@@ -9,23 +9,18 @@ except NameError:
     raise
 
 
-_jjoin = functools.partial(join, _this_dir, "json")
+_data_dir_join = functools.partial(join, _this_dir, "json")
+_text_dir_join = _data_dir_join
 
 
 def load_fld_location(get_names: bool = True) -> list[dict]:
-    with open(_jjoin("FLD_Location.json")) as f:
-        data = json.load(f)
     if get_names:
-        names = _load_textlist("fieldnamelist_ms")
-        for location in data:
-            name_id = location["Loc_name"]
-            location["Loc_name"] = names[name_id]
-    return data
+        get_names = "fieldnamelist_ms"
+    return load_with_text_field("FLD_Location", get_names, "Loc_name")
 
 
 def load_fnetveinlist(get_names: bool = True) -> list[dict]:
-    with open(_jjoin("FnetVeinList.json")) as f:
-        data = json.load(f)
+    data = load_with_text_field("FnetVeinList")
     if get_names:
         locations = {loc["id"]: loc["Loc_name"] for loc in load_fld_location()}
         for site in data:
@@ -38,21 +33,46 @@ def load_fnetveinlist(get_names: bool = True) -> list[dict]:
 
 
 def load_itm_beaconlist(get_names: bool = True):
-    with open(_jjoin("ITM_BeaconList.json")) as f:
-        data = json.load(f)
+    data = load_with_text_field("ITM_BeaconList")
     if get_names:
-        names = _load_textlist("ITM_BeaconList_ms")
+        names = load_textlist("ITM_BeaconList_ms")
         for probe in data:
             probe["Name"] = names[probe["Name"]]
             probe["Caption"] = names[probe["Caption"]]
     return data
 
 
-def _load_textlist(textlist_file) -> dict[int, str]:
+def load_with_text_field(
+    data_table: str, text_table: str = "", text_field: str = "Name"
+) -> list[dict]:
+    """Generic function to load a JSON dump and replace text fields"""
+    with open(_data_dir_join(f"{data_table}.json")) as f:
+        data = json.load(f)
+    if text_table:
+        names = load_textlist(text_table)
+        for item in data:
+            item[text_field] = names[item[text_field]]
+    return data
+
+
+def load_textlist(textlist_file) -> dict[int, str]:
     """Load textlist_file.json
 
     Returns a dictionary mapping ids to strings
     """
-    with open(_jjoin(f"{textlist_file}.json")) as f:
+    with open(_text_dir_join(f"{textlist_file}.json")) as f:
         data = json.load(f)
     return {d["id"]: d["name"] for d in data}
+
+
+def set_data_dir(data_path: str):
+    global _data_dir_join
+    if exists(data_path):
+        _data_dir_join = functools.partial(join, data_path)
+
+
+def set_text_dir(data_path: str):
+    global _text_dir_join
+    if exists(data_path):
+        _text_dir_join = functools.partial(join, data_path)
+
